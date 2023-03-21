ae363bbed8c38f85f662b1270a720cfedcef5731
Matt Reed <matthewreed26@gmail.com>
2023-03-20 21:16:55 -0400

    Apply react-core module

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,71 +1,19 @@
 module.exports = {
-  root: true,
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-  ],
-  plugins: ['prettier', '@typescript-eslint'],
-  rules: {
-    quotes: ['error', 'single', { avoidEscape: true }],
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'always-multiline',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
-      },
-    ],
-    'import/order': [
-      'error',
-      {
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-      },
-    ],
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'es5',
-        printWidth: 140,
-      },
-    ],
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-      },
-    },
-    'import/core-modules': ['sinon'],
-  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
   parserOptions: {
-    parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  overrides: [
-    {
-      files: ['**/__tests__/*.{j,t}s?(x)', '**/*.spec.{j,t}s?(x)'],
-      env: {
-        jest: true,
-      },
-    },
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      rules: {
-        'no-unused-vars': ['off'],
-        'no-undef': ['off'],
-      },
-    },
-  ],
+  plugins: ["react"],
+  ignorePatterns: ["node_modules/"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-no-target-blank": "off",
+  }
 };
